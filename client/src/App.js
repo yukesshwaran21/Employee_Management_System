c0f81314d469beade342a37b6c53330106e6316d
yukesshwaran21 <yukesshwaran6@gmail.com>
2025-09-01 13:24:55 +0530

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/*" element={<AdminDashboard />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/attendance" element={<AttendancePage />} />
           <Route path="/payroll" element={<PayrollPage />} />
